38d95eb4169bdfd3bd5ed9bdf658018bbab056c6
Samia <samiamub786@gmail.com>
2026-07-12 20:48:34 +0500

    Update intruder_system.ino

--- intruder_system.ino
+++ intruder_system.ino
@@ -5,9 +5,8 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int trigPin = 2;
 int echoPin = 3;
-int ledPin = 6;     // RED LED Pin 6 par hai
-int buzzerPin = 8;  // Buzzer Pin 8 par hai
-
+int ledPin = 6;     
+int buzzerPin = 8;  
 long duration;
 int distance;
 
@@ -25,7 +24,6 @@ void setup() {
 }
 
 void loop() {
-  // Distance calculate karna
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH); 
@@ -34,26 +32,23 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
 
-  // LCD par normal state dikhana
   lcd.setCursor(0, 0);
   lcd.print("Status: SECURE  ");
   lcd.setCursor(0, 1);
   lcd.print("Dist: "); lcd.print(distance); lcd.print(" cm    ");
 
-  // 🚨 AGAR KOI 15cm KE ANDAR AAYE (Intruder Alert!)
   if (distance > 0 && distance < 15) {
     lcd.setCursor(0, 0);
     lcd.print("Status: ALERT!! ");
     
-    digitalWrite(ledPin, HIGH);  // Red LED ON
-    tone(buzzerPin, 1200, 100);  // Beep
+    digitalWrite(ledPin, HIGH);   
+    tone(buzzerPin, 1200, 100);  
     delay(100);
     
-    digitalWrite(ledPin, LOW);   // Red LED OFF (Blinking effect)
+    digitalWrite(ledPin, LOW);   
     noTone(buzzerPin);
     delay(100);
   } else {
-    // Agar sab clear hai toh LED aur Buzzer band
     digitalWrite(ledPin, LOW);
     noTone(buzzerPin);
     delay(200); 
